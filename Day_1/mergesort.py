b44f29899a6b04ccca5bb38e90fe3a2db605a87f
Alogarithm_Ke <derrickmoio92@gmail.com>
2025-02-06 18:12:03 +0300

    merge sort

---
+++ Day_1/mergesort.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        
+        k = m + n
+
+        num3 = nums1 + nums2
+        
+        print(num3)
+        
+        
+        
+def main():
+    nums1 = [1,2,3,0,0,0]
+    m = 3
+    nums2 = [2,5,6]
+    n = 3
+    Solution().merge(nums1, m, nums2, n)
+    print(nums1)
